02d0bbd814f349d1f3c3ec4b5db346ac3823226f
bew32 <80671223+bew32@users.noreply.github.com>
2022-09-01 10:28:24 +0700

    Add files via upload

---
+++ src/components/MainHeader.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+import Image from '../images/main_header.png'
+
+const MainHeader = () => {
+  return (
+    <header className="main__header">
+      <div className="container main__header-container">
+        <div className="main__header-left">
+          <h4>#100DaysOfWorkout</h4>
+          <h1>Join The Legends Of The Fitness World</h1>
+          <p>
+          There are many variations of passages of Lorem Ipsum available, 
+          but the majority have suffered alteration in some form, by injected
+          </p>
+          <Link to="/plans" className='btn'>Get Started</Link>
+        </div>
+        <div className="main__header-right">
+          <div className='main__header-circle'></div>
+            <div className='main__header-image'>
+                <img src={Image} alt="Main Header Image" />
+            </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default MainHeader
